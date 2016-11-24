3ba126270fd325bfd311f581a061f5007a9d34f6
seiyamiyaoka <seiyamiyaoka@gmail.com>
2016-11-24 19:01:56 +0900

    デザイン微修正

--- app/models/promise.rb
+++ app/models/promise.rb
@@ -6,7 +6,7 @@ class Promise < ActiveRecord::Base
   validate :before_limit_time
 
   def before_limit_time
-    unless limit_time > promise_time
+    if limit_time > promise_time
       errors.add(:limit_time, "締め切り日は開始日より後に設定していください")
     end
   end
